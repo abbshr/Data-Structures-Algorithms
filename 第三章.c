49e5b4b32403625532500d2b0c49f045e0fb593a
Soufii <abbshr@outlook.com>
2013-11-07 00:58:49 +0800

    第三章

--- 第三章.c
+++ 第三章.c
@@ -42,7 +42,35 @@ int findMax(Tree* t, int length)
 MW[tree] = findMax(tree, 0);
 
 //16
+void inorderMorrisTraversal(TreeNode *root) {
+    TreeNode *cur = root, *prev = NULL;
+    while (cur != NULL)
+    {
+        if (cur->left == NULL)
+        {
+            printf("%d ", cur->val);
+            cur = cur->right;
+        }
+        else
+        {
+            prev = cur->left;
+            while (prev->right != NULL && prev->right != cur)
+                prev = prev->right;
 
+            if (prev->right == NULL)
+            {
+                prev->right = cur;
+                cur = cur->left;
+            }
+            else                    
+            {
+                prev->right = NULL;
+                printf("%d ", cur->val);
+                cur = cur->right;
+            }
+        }
+    }
+}
 
 //34
 void ite(Tree* t, int isLeft)
@@ -63,6 +91,9 @@ void ite(Tree* t, int isLeft)
 ite(root, 0);
 
 //36
+#define n 6
+#define m 2*n - 1
+
 typedef struct {
 	double weight;
 	int left;
@@ -70,22 +101,43 @@ typedef struct {
 	int parent
 } Node;
 
-Node huffmanT[6];
+Node huffmanT[m];
 
 void selectMin(huffmanT t, int n, int &p1, int &p2)
 {
 	int i, j;
-	for (i = 1;)
-}
-
-void initHT(huffmanT T)
-{
-
+	for (i = 1; i <= n; i++)
+		if (T[i].parent == -1)
+		{
+			p1 = i;
+			break;
+		}
+	for (j = i + 1; j <= n; j++)
+		if (T[j].parent == -1)
+		{
+			p2 = j;
+			break;
+		}
+	for (i = 1; i <= n; i++)
+		if (T[p1].weight > T[i].weight && T[i].parent == -1 && p2 != i)
+			p1 = i;
+	for (j = 1; j <= n; j++)
+		if (T[p2].weight > T[j].weight && T[j].parent == -1 && p1 != j)
+			p2 = j;
 }
 
 void createHT(huffmanT T)
 {
-
+	int i, p1, p2;
+	initHT(T);
+	for (i = n; i <= m; i++)
+	{
+		selectMin(T, i-1, p1, p2);
+		T[p1].parent = T[p2].parent = i;
+		T[i].left = p1;
+		T[i].right = p2;
+		T[i].weight = T[p1].weight + T[p2].weight;
+	}
 }
 
 void main()
@@ -93,5 +145,18 @@ void main()
 	initHT(huffmanT);
 	createHT(huffmanT);
 }
+
+huffman tree如下:				
+				   1
+				   /\
+			   0.73  0.27
+			   /\
+			0.5  0.23
+			/\
+		0.28  0.22 
+		/\
+    0.16  0.12	 
+	/\
+0.07  0.09
 
 平均编码长度为：0.27 + 0.23*2 + 0.22*3 + 0.12*4 + (0.09 + 0.07)*5 = 2.67
